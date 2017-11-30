12423fdbcfd572375d108aca7965ab2479ae88c9
daniel-certa-1228 <mfdancerta@gmail.com>
2017-11-30 13:27:13 -0600

    change sizes of photos

--- app/models/doc.rb
+++ app/models/doc.rb
@@ -1,5 +1,5 @@
 class Doc < ApplicationRecord
-    has_attached_file :avatar, styles: { small: "100x150", med: "300x450", large: "900x1200" }
+    has_attached_file :avatar, styles: {  med: "300x450", original: "900x1200" }
     validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
     def self.fuzzy_content_search(search_string)
