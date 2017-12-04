646ae661997ad564f89dc8b85e01a00e64b94fb7
daniel-certa-1228 <mfdancerta@gmail.com>
2017-12-04 15:42:14 -0600

    check if works

--- app/mailers/doc_mailer.rb
+++ app/mailers/doc_mailer.rb
@@ -1,7 +1,7 @@
 class DocMailer < ApplicationMailer
 
     def doc_mail(address, description, content, attachment)
-        attachments["#{attachment}.pdf"] = File.read( "#{Rails.root}/#{attachment}.pdf")
+        attachments["#{attachment}.pdf"] = File.read( "../#{Rails.root}/#{attachment}.pdf")
         @address = address
         @description = description
         @content = content
