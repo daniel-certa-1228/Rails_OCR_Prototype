542705ca3a632d851269bf2be7ea3bf0a9b00356
daniel-certa-1228 <mfdancerta@gmail.com>
2017-12-04 12:23:19 -0600

    mailer sending doc contents and title

--- app/mailers/doc_mailer.rb
+++ app/mailers/doc_mailer.rb
@@ -1,7 +1,9 @@
 class DocMailer < ApplicationMailer
 
-    def doc_mail(address)
+    def doc_mail(address, description, content)
         @address = address
+        @description = description
+        @content = content
         mail(to: @address, subject: 'This is a test.')
     end
 
