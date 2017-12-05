95667bd58963ffe3ad115b9b2e1ebb808ebbbd97
daniel-certa-1228 <mfdancerta@gmail.com>
2017-12-04 21:11:26 -0600

    declare ruby version

--- app/mailers/doc_mailer.rb
+++ app/mailers/doc_mailer.rb
@@ -1,6 +1,7 @@
 class DocMailer < ApplicationMailer
 
     def doc_mail(address, description, content, attachment)
+                                                    #the file needs an extra ../ on Heroku 
         attachments["#{attachment}.pdf"] = File.read( "../#{Rails.root}/#{attachment}.pdf")
         @address = address
         @description = description
